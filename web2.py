5906187bb941cb718c8cd4715c34d7ef6b892406
papasmf1 <papasmf1@gmail.com>
2022-03-31 10:38:35 +0900

    웹툰 리스트 가져오기

--- web2.py
+++ web2.py
@@ -4,7 +4,6 @@ import urllib.request
 #크롤링에 사용
 from bs4 import BeautifulSoup
 
-
 data = urllib.request.urlopen("http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
 soup = BeautifulSoup(data, "html.parser")
 
@@ -12,11 +11,16 @@ soup = BeautifulSoup(data, "html.parser")
 # <td class="title">
 # 	<a href="/webtoon/detail?titleId=20853">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
 # </td>
-cartoons = soup.find_all("p", class_="title")
+cartoons = soup.find_all("td", class_="title")
 print("개수:{0}".format(len(cartoons)))
 title = cartoons[0].find("a").text 
 link = cartoons[0].find("a")["href"]
 print(title)
 print(link)
 
+for item in cartoons:
+    title = item.text.strip()
+    print(title)
+
+    
 
